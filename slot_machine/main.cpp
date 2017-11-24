8b3534b885fb641c32b1e202a18187927c2f3521
Mert Kaya <30465847+sertcast@users.noreply.github.com>
2017-11-23 21:19:50 -0500

    Initial Commit

---
+++ slot_machine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  slot_machine
+//
+//  Created by Mert Kaya on 2017-11-23.
+//  Copyright © 2017 Mert Kaya. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
